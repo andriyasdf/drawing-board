8187fdbb50956e2fa47796f6411edd2c1fab93e2
Andriy Massimilla <andriymassimilla@gmail.com>
2018-01-29 10:33:00 -0500

    Fix order of rectangle points

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,7 +2,7 @@ import java.awt.geom.Point2D;
 
 public class Rectangle extends Shape {
     public Rectangle(int x, int y, int width, int height) {
-        super(new Point2D.Double(x, y), new Point2D.Double(x + width, y + height),
-                new Point2D.Double(x, y + height), new Point2D.Double(x + width, y));
+        super(new Point2D.Double(x, y), new Point2D.Double(x + width, y),
+                new Point2D.Double(x + width, y + height), new Point2D.Double(x, y + height));
     }
 }
