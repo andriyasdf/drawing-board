7cee6308889c5c96882d400826610d3cc9eb618f
Daniel Heidorn <daniel.j.heidorn@gmail.com>
2018-01-30 11:20:24 -0500

    Fix within method

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         drawing.add(new Rectangle(20, 20, 400, 100));
         drawing.add(new Rectangle(100, 400, 40, 40, 0x00FFFF33));
         drawing.add(new Rectangle(500, 300, 100, 200, 0x0068FEB0));
-
+        drawing.add(new Rectangle(600, 900, 100, 200, 0x0068FEB0));
         drawing.add(new ShapeGroup());
 
         drawing.show();
