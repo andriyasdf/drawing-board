f5954c43200b183e078c9430d04ebad2eba19f13
Andriy Massimilla <andriymassimilla@gmail.com>
2018-01-30 11:59:15 -0500

    Finish right triangle implementation

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,12 @@ public class Main {
 
         drawing.add(new Rectangle(20, 20, 400, 100));
         drawing.add(new Rectangle(100, 400, 40, 40, 0x00FFFF33));
-        drawing.add(new Rectangle(500, 300, 100, 200, 0x0068FEB0));
+        drawing.add(new Rectangle(550, 300, 100, 200, 0x0068FEB0));
 
-        drawing.add(new ShapeGroup());
+        drawing.add(new RightTriangle(300, 300, 100, 150, 0xFF2373b4));
+        drawing.add(new RightTriangle(500, 200, 100, -80));
+        drawing.add(new RightTriangle(400, 200, -100, 80));
+        drawing.add(new RightTriangle(525, 325, -100, -80));
 
         drawing.show();
     }
--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -2,10 +2,61 @@ import java.awt.*;
 import java.awt.geom.Point2D;
 
 public class RightTriangle extends Shape {
+    private int width;
+    private int height;
+
+    public RightTriangle(int x, int y, int width, int height) {
+        super(new Point2D.Double(x, y), new Point2D.Double(x + width, y), new Point2D.Double(x, y + height));
+        this.color = color;
+        this.width = width;
+        this.height = height;
+    }
+
     public RightTriangle(int x, int y, int width, int height, int color) {
-        super(new Point2D.Double(x, y), new Point2D.Double(x-height, y), new Point2D.Double(x, y+width));
+        this(x, y, width, height);
         this.color = color;
     }
 
+    @Override
+    public void draw(DrawingBoard db) {
+        if (width > 0) {
+            double slope = (points.get(1).getX() - points.get(0).getX())/Math.abs(height);
+            double draw = points.get(1).getX();
 
+            if (height > 0) {
+                for (int y = (int)points.get(0).getY(); y < (int)points.get(2).getY(); y++) {
+                    for (int x = (int)points.get(0).getX(); x < (int)draw; x++) {
+                        db.imgArray[y][x] = color;
+                    }
+                    draw -= slope;
+                }
+            } else {
+                for (int y = (int)points.get(0).getY(); y > (int)points.get(2).getY(); y--) {
+                    for (int x = (int)points.get(0).getX(); x < (int)draw; x++) {
+                        db.imgArray[y][x] = color;
+                    }
+                    draw -= slope;
+                }
+            }
+        } else {
+            double slope = (points.get(1).getX() - points.get(0).getX())/Math.abs(height);
+            double draw = points.get(1).getX();
+
+            if (height > 0) {
+                for (int y = (int)points.get(0).getY(); y < (int)points.get(2).getY(); y++) {
+                    for (int x = (int)points.get(0).getX(); x > (int)draw; x--) {
+                        db.imgArray[y][x] = color;
+                    }
+                    draw -= slope;
+                }
+            } else {
+                for (int y = (int)points.get(0).getY(); y > (int)points.get(2).getY(); y--) {
+                    for (int x = (int)points.get(0).getX(); x > (int)draw; x--) {
+                        db.imgArray[y][x] = color;
+                    }
+                    draw -= slope;
+                }
+            }
+        }
+    }
 }
