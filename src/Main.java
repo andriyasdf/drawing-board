a2d1875ad5409848e4721e856d82257a341f2c79
Andriy Massimilla <andriymassimilla@gmail.com>
2018-02-01 10:50:56 -0500

    Draw a house

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,17 @@
-import java.awt.geom.Point2D;
-
 public class Main {
     public static void main(String[] args) {
-        Drawing drawing = new Drawing(1000, 800);
-
         drawScene();
-    }
-
-    private static void drawTest() {
-
     }
 
     private static void drawScene() {
         Drawing drawing = new Drawing(800, 600);
 
-        ShapeGroup house = new ShapeGroup(
-                new Rectangle(300, 300, 200, 200),
-                new RightTriangle(300, 300, -100, 80),
-                new RightTriangle(300, 300, 100, 80)
-        );
+        drawing.add(new ShapeGroup(
+                new Rectangle(200, 300, 200, 125),
+                new Rectangle(350, 220, 30, 80),
+                new RightTriangle(300, 300, -110, -80),
+                new RightTriangle(300, 300, 110, -80)
+        ));
 
         drawing.show();
     }
